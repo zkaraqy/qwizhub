ad1d9df83acd31f3b3558ec13acbb3d437bdc3a8
Muhammad Azka Raki <azkaraqy552@gmail.com>
2026-09-13 16:25:43 +0800

    [QS] Fix <Kuesioner> : Default tab kuesioner jadi info dan penyesuaian navbar dan sidebar

--- app/components/layout/AppSidebar.vue
+++ app/components/layout/AppSidebar.vue
@@ -35,15 +35,6 @@
       My Surveys
     </NuxtLink>
 
-    <a href="#" class="list-group-item list-group-item-action" :class="{ active: activeItem === 'analytics' }">
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bar-chart me-2"
-        viewBox="0 0 16 16">
-        <path
-          d="M4 11H2v3h2zm5-4H7v7h2zm5-5v12h-2V2zm-2-1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM6 7a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1zm-5 4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1z" />
-      </svg>
-      Analytics
-    </a>
-
     <!-- Top Up Token AI (Peneliti only) -->
     <template v-if="user?.role === 'peneliti'">
       <NuxtLink to="/payments" class="list-group-item list-group-item-action"
